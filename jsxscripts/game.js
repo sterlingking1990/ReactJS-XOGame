5788d3ee9eea9f62863ab1666decaf6a0ac5c0aa
Rails Udemy Course <izundukingsleyemeka@gmail.com>
2019-10-07 09:41:28 +0100

    set components for winnerstatus who to play next and play buttons

--- jsxscripts/game.js
+++ jsxscripts/game.js
@@ -10,6 +10,20 @@ const gameChart={
     9:[9,6,3,5,1,8,7]
 }
 
+function StatusForWinner(props){
+    if(props.winner==''){
+        return null
+    }
+    else{
+        return (
+            <div>
+                <p>Winner is {props.winner}</p>
+                <button onClick={this.restartGame}>Restart Game</button>
+            </div>
+        )
+    }
+}
+
 class XOGame extends React.component{
 
     constructor(props){
@@ -20,12 +34,53 @@ class XOGame extends React.component{
             player_o:[],
             isToPlay:'x',
             winner:'',
-            played:''
+            played:'',
+            restart:1
         }
+
+        this.restartGame=this.restartGame.bind(this);
+    }
+
+    restartGame(){
+        this.setState({restart:1,winner:''});
     }
 
     render(){
+        var player_x=this.state.player_x;
+        var player_o=this.state.player_o;
+        var played=this.state.played;
+        var lastPlayed_x=player_x[player_x.length-1];
+        var lastPlayed_o=player_o[player_o.length-1];
+        var isToPlay=this.state.isToPlay;
+        var winner=this.state.winner;
+        var restart=this.state.restart;
+
+        //function for who won
+        var whoWon=played=='x'?checkXStatus(lastPlayed_x,player_x):checkOStatus(lastPlayed_o,player_o);
+        if(whoWon!==false){
+            this.setState({ winner: whoWon, restart: 0 });
+
+        }
         
+        let gamestate;
+
+        if (restart == 0) {
+            gamestate = <StatusForWinner winner={winner} />
+
+        }
+        else{
+            gamestate = (<fragment>
+                                    <ToPlay isToPlay={isToPlay} />
+                                    <PlayButton forValues={[1, 2, 3, 4, 5, 6, 7, 8, 9]} />
+                        </fragment>)
+        }
+        
+        return(
+                <fragment>
+                    {gamestate}
+                </fragment>
+        )
+
     }
 
 }
